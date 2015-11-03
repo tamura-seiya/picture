c2e4e255811ba3833ecbf6a46214c535b7f98a37
tamura-seiya <s262113@gmail.com>
2015-11-03 22:57:53 +0800

    change UI

--- Picture/ViewController.swift
+++ Picture/ViewController.swift
@@ -14,32 +14,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     var myLabel:UILabel!
     var view1:UIView!
+    var view2:UIView!
+    var view3:UIView!
+    var viewAry: [UIView?] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        
-        view1 = UIView(frame: self.view.frame)
-        view1.backgroundColor = UIColor.orangeColor()
-        view1.layer.position = CGPointMake(self.view.frame.width/2, self.view.frame.height/2)
-        self.view.addSubview(view1)
-        
          //scrollViewを定義
          self.scrollView?.contentSize = CGSizeMake(self.scrollView.frame.width * 3, self.scrollView.frame.size.height)
         
-        // １ページ単位でスクロールさせる
-        scrollView.pagingEnabled = true
         
-        //scroll画面の初期位置
-        scrollView.contentOffset = CGPointMake(0, 0);
+        //View1を生成
+        view1 = UIView(frame: CGRectMake(0, 0, self.scrollView.frame.width, self.scrollView.frame.size.height))
 
+        view1.backgroundColor = UIColor.blueColor()
         
-        view1 = UIView(frame: self.view.frame)
-        view1.backgroundColor = UIColor.orangeColor()
-        view1.layer.position = CGPointMake(self.view.frame.width/2, self.view.frame.height/2)
-        self.view.addSubview(view1)
+        // 配置する座標を設定する.(中心座標)
+        view1.layer.position = CGPoint(x: 375/2,y: 675/2)
         
+        
+        //View2を生成
+        view2 = UIView(frame: CGRectMake(0, 0, self.scrollView.frame.width, self.scrollView.frame.size.height))
+        view2.backgroundColor = UIColor.yellowColor()
+        
+        // 配置する座標を設定する.(中心座標)
+        view2.layer.position = CGPoint(x: 375/2,y: 675/2)
+        
+        view3 = UIView(frame: CGRectMake(0, 0, self.scrollView.frame.width, self.scrollView.frame.size.height))
+        
+        //background
+        view3.backgroundColor = UIColor.blackColor()
+        
+        // 配置する座標を設定する.(中心座標)
+        view3.layer.position = CGPoint(x: 375/2,y: 675/2)
+        
+        viewAry = [view1,view2,view3]
+
+        swipeGesture()
+
         
         //labelを生成
         myLabel = UILabel(frame: CGRectMake(0,0,200,50))
@@ -56,8 +70,8 @@ class ViewController: UIViewController {
         
         // Viewの背景色を青にする.
         self.view.backgroundColor = UIColor.cyanColor()
-
-        self.view.addSubview(myLabel)
+        //このコードで追加となる
+        self.view1.addSubview(myLabel)
         
         
         //上へのスワイプを定義
@@ -78,7 +92,41 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func swipeGesture(){
+        
+        // １ページ単位でスクロールさせる
+        scrollView.pagingEnabled = true
+        
+        //scroll画面の初期位置
+        scrollView.contentOffset = CGPointMake(0, 0);
+        
+        for i in 0...2 {
+            
+            var x:CGFloat = 0
+            var y:CGFloat = 0
+            
+            if i == 0 {
+                //x = 0
+            } else if i == 1 {
+                x = 375
+            } else if i == 2 {
+                x = 375*2
+            }
+            
+            // Scrollviewに追加
+            viewAry[i]!.frame = CGRectMake(x, y, self.scrollView.frame.width, self.scrollView.frame.size.height)
+            
+            // Scrollviewに追加
+            scrollView.addSubview(viewAry[i]!)
+        
 
+        }
+        
+        
+
+    }
+    
     //ここでswipeGestureを定義
     func handleSwipes(sender: UISwipeGestureRecognizer){
         if (sender.direction == .Up){
